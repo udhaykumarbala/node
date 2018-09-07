c27801a0db1c679c2eb28f082772b9df9c9438b6
udhaykumarbala <udhaykumar97@outlook.com>
2018-09-07 20:48:36 +0530

    s

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express();
 
 
 app.get('/',function(req,res){
-	res.json("hello");
+	res.end("hello");
 });
 
-app.listen(3000, function(err) {
+app.listen(80, function(err) {
 	if(err) throw err;
 	console.log("run successfully");
 });
