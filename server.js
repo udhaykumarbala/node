164c78530363758c207ae338e51e63efb85951ab
udhaykumarbala <udhaykumar97@outlook.com>
2018-09-07 18:51:46 +0530

    first added

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/name',function(req,res){
+	res.end("hello");
+});
+
+app.listen(80, function(err) {
+	if(err) throw err;
+	console.log("run successfully");
+});
